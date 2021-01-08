eb142cb21fd5f42653acf3f01e7d8fbd22c65a30
kethort <10733500+kethort@users.noreply.github.com>
2021-01-08 01:57:02 -0800

    rebuilt exe

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ import os
 
 installer_script = os.path.join(os.getcwd(), 'src\\installer\\ArduinoModbusInstaller.py')
 modbus_ini = os.path.join(os.getcwd(), 'src\\ini_settings\\modbus_ini_settings')
+modbus_ini_adc = os.path.join(os.getcwd(), 'src\\ini_settings\\modbus_ini_settings_adc')
+modbus_ini_adc_dac = os.path.join(os.getcwd(), 'src\\ini_settings\\modbus_ini_settings_adc_dac')
+modbus_ini_dac = os.path.join(os.getcwd(), 'src\\ini_settings\\modbus_ini_settings_dac')
 
-data_files = [('', [modbus_ini])]
+data_files = [('', [modbus_ini, modbus_ini_adc, modbus_ini_adc_dac, modbus_ini_dac])]
 setup(windows=[installer_script], data_files = data_files)

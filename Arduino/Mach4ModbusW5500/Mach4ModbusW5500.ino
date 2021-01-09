6eb97ca77e59ced6098c46f693780f7ac24d7660
kethort <10733500+kethort@users.noreply.github.com>
2021-01-08 23:45:00 -0800

    added better comments to Arduino sketch

--- Arduino/Mach4ModbusW5500/Mach4ModbusW5500.ino
+++ Arduino/Mach4ModbusW5500/Mach4ModbusW5500.ino
@@ -12,13 +12,13 @@
 //-------------------------------------------------------------
 // PORTA = {22, 23, 24, 25, 26, 27, 28, 29};       
 
-// PORTG = {41, 40, 39, xx, xx, 4, xx, xx};
-// PORTB = {xx, xx, xx, xx, 10, 11, 12, 13}; // w5500 ethernet module uses 53, 52, 51, and 50 
-                                             // so need to skip those pins
+// PORTG = {41, 40, 39, xx, xx, 4,  xx, xx};
+// PORTB = {xx, xx, xx, xx, 10, 11, 12, 13}; // w5500 ethernet module uses 53, 52, 51, and 50;
+                                             // cannot use those pins
 // PORTD = {21, 20, 19, 18, xx, xx, xx, 38};
 // PORTE = {xx, xx, xx, 5,  2,  3,  xx, xx};
 
-// PORTH = {17, 16, xx, 6, 7, 8, 9, xx};
+// PORTH = {17, 16, xx, 6,  7,  8,  9,  xx};
 // PORTJ = {15, 14, xx, xx, xx, xx, xx, xx};
 
 //-------------------------------------------------------------
@@ -42,7 +42,6 @@
 
 #ifdef USE_ADC
 #include <Adafruit_ADS1015.h>
-
 #define ADC_ADDR 0x48
 Adafruit_ADS1115 ads(ADC_ADDR);
 #endif
@@ -63,8 +62,12 @@ byte ip[] = {192, 168, 0, 50};
 byte pos = 0;
 
 /*
-    The modbus TCP protocol does not seem to work with register addresses that are a factor of 8. 
-    That's why they are skipped in the sketch and in the Mach4 modbus plugin configuration.
+    The modbus TCP protocol only processes register addresses 7 bytes at a time. Every eighth register
+    address (byte in the modbusRegs array) does not get sent through. 
+    That's why they are skipped in the register array and in the Mach4 modbus plugin configuration.
+
+    The bytes in the modbusRegs array does not have to be in any order, but the value of the bytes do have to be different
+    in each location to avoid collisions on the other side.  
 */
 byte modbusRegs[] = {1,  2,  3,  4,  5,  6,  7,  9,   // 0 - 7    PORTF
                      10, 11, 12, 13, 14, 15, 17, 18,  // 8 - 15   PORTK
@@ -103,15 +106,15 @@ void setup() {
   DDRA = 0xFF; 
   PORTA = 0;
   
-  DDRB = 0xF0;
-  PORTB &= ~0xF0;
+  DDRB = 0xF1; 
+  PORTB &= ~0xF1;
   
   DDRD = 0x8F;
   PORTD &= ~0x8F;
   
   DDRE = 0x38;
   PORTE &= ~0x38;
-
+  
   DDRG = 0x27;
   PORTG &= ~0x27;
   
@@ -134,6 +137,12 @@ void setup() {
   }
 }
 
+/*
+    The pos variable is used to shift the bit to the pin on the PORT. It also points to the
+    modbus register number in the modbusRegs array and is used to switch to the next 
+    port after each full read/write of the 8-bits in the port is finished. This allows the Arduino
+    loop() to iterate through all of the ports and pins on the device without the use of for loops.
+*/
 void loop() {  
   mb.task();
 
@@ -150,11 +159,8 @@ void loop() {
 }
 
 /*
-    The readInputs function reads the pin value from the PORT bit-map.
-
-    The pos variable acts like a pointer to the pin in the PORT. It also points to the
-    modbus register number in the modbusRegs array and is used to switch to the next 
-    port after each full read of the port occurs. 
+    The readInputs function reads the pin value from the PORT bit-map and sets the modbus
+    register to the current state.  
 */
 void readInputs() {
   byte switchState = pos / 8; // switch to a new port every 8 bits
@@ -162,18 +168,22 @@ void readInputs() {
   
   switch(switchState) {
     case 0:
+      // pins A0 - A7
       invertedState = ((PINF >> pos % 8) & 0x01) ? 0 : 1; // inputs are active low; this gets the inverted state of the pin
       mb.Coil(modbusRegs[pos], invertedState); // put the input state into the coil register
       break;
     case 1:
+      // pins A8 - A15
       invertedState = ((PINK >> pos % 8) & 0x01) ? 0 : 1;
       mb.Coil(modbusRegs[pos], invertedState);
       break;
     case 2: 
+      // pins 37 - 30
       invertedState = ((PINC >> pos % 8) & 0x01) ? 0 : 1;
       mb.Coil(modbusRegs[pos], invertedState);
       break;
     case 3:
+      // pins 49 - 42
       invertedState = ((PINL >> pos % 8) & 0x01) ? 0 : 1;
       mb.Coil(modbusRegs[pos], invertedState);
       break;
@@ -181,57 +191,73 @@ void readInputs() {
 }
 
 /*
-    The processOutputs function requires much more positional manipulation due to 
+    The processOutputs function reads the current modbus coil state in the register array and 
+    writes the value of that bit to the port.   
+    
+    This function requires much more positional manipulation through the ports due to 
     the scattered orientation of the pins across the rest of the unused ports. 
 
-    As a result, the bit manipulation is basically hard coded using various if statements, so changing
+    As a result, the bit manipulation is brute forced using various if statements, so changing
     any of the output pins might be a difficult task and isn't recommended. 
     
     The advantage to the port manipulation is much faster write access vs the Arduino digitalWrite function.
     
     Separating the input and output ports in this way was intentional, as it was the easiest way to 
-    'split' the Arduino Mega in half with inputs on one side of the PCB, and the outputs on the other side.
+    'split' the Arduino Mega in half with inputs on one side of the PCB, and the outputs on the other side 
+    while maintaining organization of the port bit-masks in the code.
 */
 void processOutputs() {
   byte switchState = pos / 8; // switch to a new port every 8 bits
 
   switch(switchState) {
-    case 4:
-      // reads the coil state in the modbus buffer and writes the value of that bit to the port 
+    case 4: // pins 22 - 29
       PORTA = mb.Coil(modbusRegs[pos]) ? PORTA | (1 << (pos % 8)) : PORTA & ~(1 << (pos % 8));
       break;
     case 5:
-      if (pos >= 40 && pos <= 42) {
+      if (pos >= 40 && pos <= 42) { 
+        // pins 41, 40 and 39
         PORTG = mb.Coil(modbusRegs[pos]) ? PORTG | (1 << (pos % 8)) : PORTG & ~(1 << (pos % 8));
-      } else if (pos == 43) {
+      }else if (pos == 43) { 
+        // pin 10
         PORTB = mb.Coil(modbusRegs[pos]) ? PORTB | (1 << ((pos + 1) % 8)) : PORTB & ~(1 << ((pos + 1) % 8));
-      } else if (pos == 44) {
+      }else if (pos == 44) { 
+        // pin 4
         PORTG = mb.Coil(modbusRegs[pos]) ? PORTG | (1 << ((pos + 1) % 8)) : PORTG & ~(1 << ((pos + 1) % 8));
-      } else if (pos >= 45 && pos <= 47) {
+      } else if (pos >= 45 && pos <= 47) { 
+        // pins 11 - 13
         PORTB = mb.Coil(modbusRegs[pos]) ? PORTB | (1 << (pos % 8)) : PORTB & ~(1 << (pos % 8));
       }
       break;
     case 6: 
-      if (pos >= 48 && pos <= 51) {
+      if (pos >= 48 && pos <= 51) { 
+        // pins 21, 20 and 19
         PORTD = mb.Coil(modbusRegs[pos]) ? PORTD | (1 << (pos % 8)) : PORTD & ~(1 << (pos % 8));
-
+        
         if (pos == 51) {
+          // pin 5
           PORTE = mb.Coil(modbusRegs[pos + 1]) ? PORTE | (1 << (pos % 8)) : PORTE & ~(1 << (pos % 8));
         }
       } else if (pos >= 52 && pos <= 53) {
+        // pins 2 and 3
         PORTE = mb.Coil(modbusRegs[pos + 1]) ? PORTE | (1 << (pos % 8)) : PORTE & ~(1 << (pos % 8));
       } else if (pos == 54) {
+        // pin 38
         PORTD = mb.Coil(modbusRegs[pos + 1]) ? PORTD | (1 << ((pos + 1) % 8)) : PORTD & ~(1 << ((pos + 1) % 8));
       }
       break;
     case 7:
       if (pos == 56) {
+        // pin 17
         PORTH = mb.Coil(modbusRegs[pos]) ? PORTH | (1 << (pos % 8)) : PORTH & ~(1 << (pos % 8));
+        // pin 15
         PORTJ = mb.Coil(modbusRegs[pos + 1]) ? PORTJ | (1 << (pos % 8)) : PORTJ & ~(1 << (pos % 8));
-      } else if (pos == 57) { 
+      } else if (pos == 57) {
+        // pin 16
         PORTH = mb.Coil(modbusRegs[pos + 1]) ? PORTH | (1 << (pos % 8)) : PORTH & ~(1 << (pos % 8));
+        // pin 14
         PORTJ = mb.Coil(modbusRegs[pos + 2]) ? PORTJ | (1 << (pos % 8)) : PORTJ & ~(1 << (pos % 8));
       } else if (pos >= 60 && pos <= 63) {
+        // pins 6 - 9
         PORTH = mb.Coil(modbusRegs[pos]) ? PORTH | (1 << ((pos - 1) % 8)) : PORTH & ~(1 << ((pos - 1) % 8));
       }
       break;
